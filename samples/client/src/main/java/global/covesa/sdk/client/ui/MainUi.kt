d8f92c79154e1bd10c1308bec54f62f3be1f4439
sim <git@sgougeon.fr>
2024-12-16 08:42:30 +0000

    Use Column for LightStates

--- samples/client/src/main/java/global/covesa/sdk/client/ui/MainUi.kt
+++ samples/client/src/main/java/global/covesa/sdk/client/ui/MainUi.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -138,24 +136,18 @@ private fun LightStates(
     allLights: List<LightState>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
+    Column(
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 8.dp)
     ) {
-        item {
-            Text(
-                text = "Current lights:\n",
-                style = MaterialTheme.typography.headlineSmall,
-                modifier = modifier
-            )
-        }
-        items(
-            items = allLights,
-            key = { it.zone },
-            contentType = { it }
-        ) {
-            LightStateRow(light = it)
+        Text(
+            text = "Current lights:\n",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = modifier
+        )
+        allLights.forEach {
+            LightStateRow(it)
         }
     }
 }
